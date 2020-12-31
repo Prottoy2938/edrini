82ea0736b314b51c2688863de53d48cfb78a72d8
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-12-31 11:23:19 +0600

    Working on the rating section

--- src/components/track-page/rating-section/rating-section.tsx
+++ src/components/track-page/rating-section/rating-section.tsx
@@ -3,12 +3,53 @@ import { Box, Text } from "@chakra-ui/react";
 import Slider, { Range } from "rc-slider";
 import "rc-slider/assets/index.css";
 
-const RatingSection = () => {
+const marks = {
+  0: "0",
+  10: "1",
+  20: {
+    style: {
+      color: "#559945",
+    },
+    label: <strong>2</strong>,
+  },
+  30: "3",
+  40: "4",
+  50: {
+    style: {
+      color: "#00ccff",
+    },
+    label: <strong>5</strong>,
+  },
+  60: "6",
+  70: "7",
+  80: {
+    style: {
+      color: "#ffa200",
+    },
+    label: <strong>8</strong>,
+  },
+  90: "9",
+  100: {
+    style: {
+      color: "#ff006f",
+    },
+    label: <strong>10</strong>,
+  },
+};
+
+const RatingSection: React.FC = () => {
   return (
     <Box mt={6}>
-      <Range />
+      <Slider
+        dots
+        min={0}
+        marks={marks}
+        step={20}
+        // onChange={log}
+        defaultValue={20}
+      />
       <Box>
-        <Text
+        {/* <Text
           fontSize="lg"
           bg="#0085f2"
           pr={1}
@@ -18,7 +59,7 @@ const RatingSection = () => {
           mt={2}
         >
           7.6
-        </Text>
+        </Text> */}
       </Box>
     </Box>
   );
