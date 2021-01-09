4863ad670532c912a6f80aadd0902842c45a8cc7
Prottay Rudra <prottoyrudra7777@gmail.com>
2021-01-09 22:40:39 +0600

    Updated the track page

--- src/components/track-page/carousel-card/carousel-card.tsx
+++ src/components/track-page/carousel-card/carousel-card.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Image, Button, Heading, Text } from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
 import Props from "./carousel-card.model";
 import { v4 as uuid } from "uuid";
 
@@ -8,33 +8,35 @@ const CarouselCard: React.FC<Props> = (props: Props) => {
   const artistNames = trackData.spotifyData.artists;
 
   return (
-    <Box
-      w="300px"
-      height="300px"
-      backgroundImage={`url(${trackData.spotifyData.album.images[0].url})`}
-      opacity="0.65"
-      _hover={{ opacity: "0.99" }}
-      cursor="pointer"
-      borderRadius={5}
-      pl={7}
-      pr={4}
-      pt="70px"
-    >
-      <Box>
-        <Heading>{trackData.spotifyData.name}</Heading>
-        <Text mt={3}>
-          {artistNames.map((artist, i) => (
-            <span key={uuid}>
-              {" "}
-              {artist.name}{" "}
-              {artistNames.length > 1 || i + 1 !== artistNames.length
-                ? ","
-                : ""}{" "}
-            </span>
-          ))}
-        </Text>
+    <a href={`/track?s=${trackData.spotifyData.id}`}>
+      <Box
+        w="300px"
+        height="300px"
+        backgroundImage={`url(${trackData.spotifyData.album.images[0].url})`}
+        opacity="0.65"
+        _hover={{ opacity: "0.99" }}
+        cursor="pointer"
+        borderRadius={5}
+        pl={7}
+        pr={4}
+        pt="70px"
+      >
+        <Box>
+          <Heading>{trackData.spotifyData.name}</Heading>
+          <Text mt={3}>
+            {artistNames.map((artist, i) => (
+              <span key={uuid}>
+                {" "}
+                {artist.name}{" "}
+                {artistNames.length > 1 || i + 1 !== artistNames.length
+                  ? ","
+                  : ""}{" "}
+              </span>
+            ))}
+          </Text>
+        </Box>
       </Box>
-    </Box>
+    </a>
   );
 };
 
